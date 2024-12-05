c19c643240a5549ab47c0c5116bffaf7b2018466
Matthew Nazari <matthewanazari@gmail.com>
2024-12-05 00:28:41 -0800

    clean scripts

--- scripts/pct.sh
+++ scripts/pct.sh
@@ -4,8 +4,6 @@
 #SBATCH -p seas_gpu
 #SBATCH -t 6:00:00         # Runtime in D-HH:MM:SS, minimum of 10 minutes
 #SBATCH --mem=6000          # Memory pool for all cores (see also --mem-per-cpu) MBs
-#SBATCH -o /n/home10/hongjinlin/outputs/cs288_pct_%j.out  # File to which STDOUT will be written, %j inserts jobid
-#SBATCH -e /n/home10/hongjinlin/outputs/cs288_pct_%j.err  # File to which STDERR will be written, %j inserts jobid
 
 set -x
 
